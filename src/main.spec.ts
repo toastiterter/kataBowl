34112691393511194b78c470c4c2a54f2c5b19ce
Rémi Bruguier <remi@gojob.com>
2021-03-28 15:50:34 +0200

    refactor: add some helper function to improve test lisibility

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -13,59 +13,34 @@ describe('BowlingGame', () => {
     });
 
     it('score is 1 when only one pin was knocked down', () => {
-      gutterThrows(19);
       bowlingGame.countThrow(1);
+      gutterThrows(19);
       expect(bowlingGame.score).toEqual(1);
     });
 
-    it('score is 11 in 4 succedeed throws', () => {
+    it('score is correct for a typical game', () => {
       bowlingGame.countThrow(5);
       bowlingGame.countThrow(3);
+      gutterThrows(10);
       bowlingGame.countThrow(2);
       bowlingGame.countThrow(1);
-      gutterThrows(16);
+      gutterThrows(6);
       expect(bowlingGame.score).toEqual(11);
-    });
-
-    it('score is 15 in 5 throws', () => {
-      bowlingGame.countThrow(5);
-      bowlingGame.countThrow(3);
-      bowlingGame.countThrow(0);
-      bowlingGame.countThrow(2);
-      bowlingGame.countThrow(5);
-      gutterThrows(15);
-      expect(bowlingGame.score).toEqual(15);
     });
   });
 
   describe('open frames and spare(s)', () => {
-    it('score is 16 with one spare and 3 pin down', () => {
-      bowlingGame.countThrow(5);
-      bowlingGame.countThrow(5);
+    it('score is correct for one spare only', () => {
+      spare(5, 5);
       bowlingGame.countThrow(3);
       gutterThrows(17);
       expect(bowlingGame.score).toEqual(16);
     });
 
-    it('score is 22 with one spare and 4 succedeed throws', () => {
-      bowlingGame.countThrow(5);
-      bowlingGame.countThrow(5);
-      bowlingGame.countThrow(0);
-      bowlingGame.countThrow(3);
-      bowlingGame.countThrow(7);
-      bowlingGame.countThrow(1);
-      bowlingGame.countThrow(1);
-      gutterThrows(13);
-      expect(bowlingGame.score).toEqual(22);
-    });
-
-    it('score is 22 with 2 spare and 3 succedeed throws', () => {
-      bowlingGame.countThrow(5);
-      bowlingGame.countThrow(5);
-      bowlingGame.countThrow(0);
-      bowlingGame.countThrow(0);
-      bowlingGame.countThrow(7);
-      bowlingGame.countThrow(3);
+    it('score is correct for multiple spares', () => {
+      spare(5, 5);
+      gutterThrows(2);
+      spare(7, 3);
       bowlingGame.countThrow(1);
       gutterThrows(13);
       expect(bowlingGame.score).toEqual(22);
@@ -73,14 +48,14 @@ describe('BowlingGame', () => {
   });
 
   describe('open frames and strike(s)', () => {
-    it('score is 10 with 1 strike', () => {
-      bowlingGame.countThrow(10);
+    it('score is correct for one strike and gutter throws only', () => {
+      strike();
       gutterThrows(19);
       expect(bowlingGame.score).toEqual(10);
     });
 
-    it('score is 16 with 1 strike and 2 throws', () => {
-      bowlingGame.countThrow(10);
+    it('score is correct with 1 strike and 2 throws', () => {
+      strike();
       bowlingGame.countThrow(1);
       bowlingGame.countThrow(2);
       gutterThrows(17);
@@ -89,21 +64,18 @@ describe('BowlingGame', () => {
   });
 
   describe('open frames, spares and strikes', () => {
-    it('score is 34 with 1 strike and 2 spares', () => {
-      bowlingGame.countThrow(10);
-      bowlingGame.countThrow(5);
-      bowlingGame.countThrow(5);
+    it('score is correct with 1 strike and 2 spares', () => {
+      strike();
+      spare(5, 5);
       bowlingGame.countThrow(2);
       expect(bowlingGame.score).toEqual(34);
     });
 
-    it('score is 70 with 2 strike and 2 spares', () => {
-      bowlingGame.countThrow(10);
-      bowlingGame.countThrow(5);
-      bowlingGame.countThrow(5);
-      bowlingGame.countThrow(10);
-      bowlingGame.countThrow(5);
-      bowlingGame.countThrow(5);
+    it('score is correct with 2 strike and 2 spares', () => {
+      strike();
+      spare(5, 5);
+      strike();
+      spare(5, 5);
       expect(bowlingGame.score).toEqual(70);
     });
   });
@@ -112,5 +84,14 @@ describe('BowlingGame', () => {
     [...Array(numberOfThrows)].map(() => {
       bowlingGame.countThrow(0);
     });
+  };
+
+  const spare = (pinsDownOnFirstThrow: number, pinsDownOnSecondThrow: number) => {
+    bowlingGame.countThrow(pinsDownOnFirstThrow);
+    bowlingGame.countThrow(pinsDownOnSecondThrow);
+  };
+
+  const strike = () => {
+    bowlingGame.countThrow(10);
   };
 });
